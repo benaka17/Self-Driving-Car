24c0a992f855a098708385bb9f6d7847b3ae5045
benaka17 <benaka@bzz.ch>
2022-06-20 16:04:01 +0200

    COMMIT FOR DEPLOY

---
+++ main.js
@@ -0,0 +1,26 @@
+// Set the road
+const canvas = document.getElementById("myCanvas");
+
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width/2, canvas.width*0.9)
+const car = new Car(road.getLaneCenter(1)+7.5,100,30,50);
+
+animate();
+
+function animate(){
+
+    car.update();
+
+    canvas.height=window.innerHeight; //canvas fills whole screen and past car places are not shown
+
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
+
+    road.draw(ctx);
+    car.draw(ctx);
+
+    ctx.restore();
+    requestAnimationFrame(animate); //calls animate function again again, creates movement illusion
+}
